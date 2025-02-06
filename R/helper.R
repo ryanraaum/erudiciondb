de3ac916d91ef491e393e25f72695b7c3bf14376
Ryan Raaum <github@raaum.org>
2025-02-06 11:58:28 -0500

    import and test .initials function

--- R/helper.R
+++ R/helper.R
@@ -1,4 +1,31 @@
 
+## data helpers
+
+# starting point derived from https://github.com/rijpma/capelinker (no LICENSE)
+# - among other changes, now
+#   - handles hyphenated names
+#   - handles accented names
+.initials <- function(strings, return_NA_on_empty = FALSE){
+  out = stringi::stri_extract_all_regex(
+    str = strings,
+    pattern = "^[A-Za-zÀ-ÖØ-öø-ÿ]|(?:\\-)[A-Za-zÀ-ÖØ-öø-ÿ]|\\s[A-Za-zÀ-ÖØ-öø-ÿ]|[.][A-Za-zÀ-ÖØ-öø-ÿ]",
+    simplify = FALSE,
+    omit_no_match = TRUE) # if TRUE, empty list -> "" below
+
+  out = lapply(out, stringi::stri_replace_all_regex, "[ .]", "")
+
+  if(return_NA_on_empty){
+    out[sapply(out, length) == 0] = NA
+  } else if (!return_NA_on_empty){
+    out[sapply(out, length) == 0] = ""
+  }
+
+  sapply(out, stringi::stri_join, collapse = "")
+}
+
+
+## test helpers
+
 make_testcon <- function(dbtype="sqlite", env=parent.frame()) {
   if (dbtype == "sqlite") {
     testcon <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
@@ -17,3 +44,4 @@ make_testcon <- function(dbtype="sqlite", env=parent.frame()) {
 table_columns <- function(con, table_name) {
   DBI::dbListFields(con, table_name)
 }
+
---
+++ tests/testthat/test-helper.R
@@ -0,0 +1,15 @@
+
+test_that(".initials makes initials", {
+  expect_equal(.initials("Ashley"), "A")
+  expect_equal(.initials("Ashley Victoria"), "AV")
+  expect_equal(.initials(c("Ashley V.")), "AV")
+  expect_equal(.initials(c("A. V.")), "AV")
+  expect_equal(.initials(c("A V")), "AV")
+  expect_equal(.initials("Ashley Victoria Priscilla"), "AVP")
+  expect_equal(.initials("Åshley Victoria"), "ÅV")
+  expect_equal(.initials("Åshley Éxperion"), "ÅÉ")
+  expect_equal(.initials("Åshley Éxperion"), "ÅÉ")
+  expect_equal(.initials("Bah-Humbug"), "B-H")
+})
+
+
