32626c556dde553df05b551d9f1b5a4aefb90c1b
Ryan Raaum <github@raaum.org>
2025-02-06 15:17:55 -0500

    import .augment_person and tests

---
+++ R/augmentors.R
@@ -0,0 +1,56 @@
+AUGMENTORS <- list()
+
+.augment_person <- function(new_p) {
+  if (is.na(new_p$also_known_as)) {
+    new_p$also_known_as <- FALSE
+  }
+  if (is.na(new_p$comma_suffix)) {
+    new_p$comma_suffix <- FALSE
+  }
+  if (is.na(new_p$ascii_given_names)) {
+    given_names <- c(new_p$primary_given_names, new_p$other_given_names)
+    given_names <- given_names[!is.na(given_names)]
+    given_names <- paste(given_names, collapse=" ")
+    new_p$ascii_given_names <- tolower(stringi::stri_trans_general(given_names, id = "Latin-ASCII"))
+  }
+  if (is.na(new_p$ascii_surnames)) {
+    # NOTE: not including particles, unclear if that is the right move
+    new_p$ascii_surnames <- tolower(stringi::stri_trans_general(new_p$surnames, id = "Latin-ASCII"))
+  }
+  if (is.na(new_p$short_referent)) {
+    new_p$short_referent <- new_p$primary_given_names
+  }
+  if (is.na(new_p$long_referent)) {
+    all <- c(
+      new_p$prefix,
+      new_p$primary_given_names,
+      .initials(new_p$other_given_names),
+      new_p$dropping_particle,
+      new_p$non_dropping_particle,
+      new_p$surnames,
+      ifelse(new_p$comma_suffix, ",", NA),
+      new_p$suffix
+    )
+    all <- all[!is.na(all)]
+    new_p$long_referent <- paste(all, collapse = " ") |>
+      stringr::str_replace(" , ", ", ") # get rid of extra space before comma suffix if necessary
+  }
+  if (is.na(new_p$sorting_referent)) {
+    if (is.na(new_p$surnames)) {
+      # if no surnames, then mononym? so use given names as sorting?
+      new_p$sorting_referent <- stringi::stri_trans_general(new_p$primary_given_names, "latin-ascii")
+    } else {
+      # include non-dropping particle
+      sorting_surname <- c(new_p$non_dropping_particle, new_p$surnames)
+      sorting_surname <- sorting_surname[!is.na(sorting_surname)]
+      if (!is.na(new_p$non_dropping_particle) && endsWith(new_p$non_dropping_particle, "-")) {
+        sorting_surname <- paste(sorting_surname, collapse="")
+      } else {
+        sorting_surname <- paste(sorting_surname, collapse=" ")
+      }
+      new_p$sorting_referent <- tolower(stringi::stri_trans_general(sorting_surname, "latin-ascii"))
+    }
+  }
+  new_p
+}
+AUGMENTORS[["person"]] <- .augment_person
---
+++ tests/testthat/test-augmentors.R
@@ -0,0 +1,49 @@
+
+test_that(".augment_person works", {
+  testcon <- make_testcon("duckdb")
+  expect_no_error(edb_create_tables(testcon))
+
+  proto_new_person_1 <- list(primary_given_names = "Tammy",
+                           other_given_names = "Sue",
+                           surnames = "Sweetie")
+  new_person_1 <- expect_no_condition(.new_object("person", proto_new_person_1, testcon))
+
+  augmented_person_1 <- expect_no_condition(.augment_person(new_person_1))
+
+  expect_false(augmented_person_1$also_known_as)
+  expect_false(augmented_person_1$comma_suffix)
+  expect_equal(augmented_person_1$ascii_given_names, "tammy sue")
+  expect_equal(augmented_person_1$ascii_surnames, "sweetie")
+  expect_equal(augmented_person_1$short_referent, "Tammy")
+  expect_equal(augmented_person_1$long_referent, "Tammy S Sweetie")
+  expect_equal(augmented_person_1$sorting_referent, "sweetie")
+
+  proto_new_person_2 <- list(primary_given_names = "Émily Sûe",
+                             surnames = "Žoe")
+  new_person_2 <- expect_no_condition(.new_object("person", proto_new_person_2, testcon))
+
+  augmented_person_2 <- expect_no_condition(.augment_person(new_person_2))
+
+  expect_false(augmented_person_2$also_known_as)
+  expect_false(augmented_person_2$comma_suffix)
+  expect_equal(augmented_person_2$ascii_given_names, "emily sue")
+  expect_equal(augmented_person_2$ascii_surnames, "zoe")
+  expect_equal(augmented_person_2$short_referent, "Émily Sûe")
+  expect_equal(augmented_person_2$long_referent, "Émily Sûe Žoe")
+  expect_equal(augmented_person_2$sorting_referent, "zoe")
+
+  proto_new_person_3 <- list(primary_given_names = "Juan",
+                             non_dropping_particle = "de la",
+                             surnames = "Roche")
+  new_person_3 <- expect_no_condition(.new_object("person", proto_new_person_3, testcon))
+
+  augmented_person_3 <- expect_no_condition(.augment_person(new_person_3))
+
+  expect_false(augmented_person_3$also_known_as)
+  expect_false(augmented_person_3$comma_suffix)
+  expect_equal(augmented_person_3$ascii_given_names, "juan")
+  expect_equal(augmented_person_3$ascii_surnames, "roche")
+  expect_equal(augmented_person_3$short_referent, "Juan")
+  expect_equal(augmented_person_3$long_referent, "Juan de la Roche")
+  expect_equal(augmented_person_3$sorting_referent, "de la roche")
+})
